91cb16720c90cb48b90f563f878ff9fd62503dfc
Michal Trubini <mchl.trubini@gmail.com>
2021-11-30 07:27:46 +0100

    js validation WIP

--- js/script.js
+++ js/script.js
@@ -1,14 +1,30 @@
 let submitButton = document.querySelector('.contact__button');
 let input = document.querySelectorAll('input');
+let errorMessages = {
+  1: 'First Name cannot be empty',
+  2: 'Last Name cannot be empty',
+  3: 'Looks like this is not an email',
+  4: 'Password cannot be empty'
+}
 
-submitButton.addEventListener('click',checkValidityAll);
+submitButton.addEventListener('click',errorAll);
 
-function checkValidityAll() {
+function errorAll() {
 
-  input.forEach(checkValiditySpecific);
+  input.forEach(errorSpecific);
 
-  function checkValiditySpecific(item) {
+  function errorSpecific(item) {
+    if(!item.validity.valid) {
+      let dataAttribute = item.getAttribute('data-errorId');
+      let errorIcon = document.querySelector(`.contact__error-icon[data-errorId='${dataAttribute}']`);
+      let errorMessage = document.querySelector(`.contact__error-text[data-errorId='${dataAttribute}']`);
 
+/*       let inputWithError = document.querySelector(`input[data-errorId='${dataAttribute}']`); */
+
+      item.classList.add('input-error-outline');
+      errorIcon.classList.add('contact__error-icon-visible');
+      errorMessage.innerText = errorMessages[dataAttribute];
+      errorMessage.classList.add('contact__error-text-visible');
+    }
   }
-
 }
